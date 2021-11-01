9164356656c1ee4d5601fe491f41701e514ec8a6
John Doe <you889@hanmail.net>
2021-11-02 00:47:01 +0900

    add Thread

--- src/j1030_basic_java/StudyThread.java
+++ src/j1030_basic_java/StudyThread.java
@@ -6,15 +6,44 @@ public class StudyThread {
 
 	public static void main(String[] args) {
 		
-		
 		System.out.println("1");
 		
-		new Thread(new Runnable()) {
-
-		};	
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+               for (int i = 0; i < 5; i++) { 
+				try {
+					Thread.sleep(100);
+					System.out.println(Thread.currentThread().getName() + ":" + i);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+               }
+				//System.out.println("3");		
+			}
+			
+		}).start();
+		
+		
+		// 추상메서드가 하나인 인터페이스 에서는 자바8 제공의 람다식 표현법을 쓸 수 있음. 위와 동일 코드.   
+		new Thread(() -> {
+	
+           for (int i = 0; i < 5; i++) { 
+				try {
+					Thread.sleep(100);
+					System.out.println(Thread.currentThread().getName() + ":" + i);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				//System.out.println("3");		
+			}
+			
+		}).start();
 		
 		System.out.println("2");
-
-	}
+		
+	} 
 
 }
+	
